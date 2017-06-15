97aacf351854576022a2a652d22c779374b1a0c8
tterrag1098 <tterrag1098@gmail.com>
2017-06-15 11:55:31 -0400

    Add @API annotations into the API

---
+++ src/main/java/team/chisel/ctm/api/model/package-info.java
@@ -0,0 +1,6 @@
+@API(apiVersion = CTMAPIProps.VERSION, owner = "ctm", provides = "ctm-api-models")
+package team.chisel.ctm.api.model;
+
+import net.minecraftforge.fml.common.API;
+import team.chisel.ctm.api.CTMAPIProps;
+
