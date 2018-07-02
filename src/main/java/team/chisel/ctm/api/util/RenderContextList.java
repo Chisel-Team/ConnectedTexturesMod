65618f5593d7fb5180be35eb5c5f45319f32ffc4
tterrag1098 <tterrag1098@gmail.com>
2018-07-02 00:30:46 -0400

    #69 Move to guava cache for RegionCache to avoid strong value references

--- src/main/java/team/chisel/ctm/api/util/RenderContextList.java
+++ src/main/java/team/chisel/ctm/api/util/RenderContextList.java
@@ -1,25 +1,30 @@
 package team.chisel.ctm.api.util;
 
+import gnu.trove.map.TObjectLongMap;
+import gnu.trove.map.custom_hash.TObjectLongCustomHashMap;
+import gnu.trove.strategy.IdentityHashingStrategy;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.WeakHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import com.google.common.collect.Maps;
-
-import gnu.trove.map.TObjectLongMap;
-import gnu.trove.map.custom_hash.TObjectLongCustomHashMap;
-import gnu.trove.strategy.IdentityHashingStrategy;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import team.chisel.ctm.CTM;
 import team.chisel.ctm.api.texture.ICTMTexture;
 import team.chisel.ctm.api.texture.ITextureContext;
 import team.chisel.ctm.api.texture.ITextureType;
 import team.chisel.ctm.client.util.RegionCache;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Maps;
 
 /**
  * List of IBlockRenderContext's
@@ -27,16 +32,27 @@ import team.chisel.ctm.client.util.RegionCache;
 @ParametersAreNonnullByDefault
 public class RenderContextList {
     
-    private static final ThreadLocal<WeakHashMap<IBlockAccess, RegionCache>> regionMetaCache = ThreadLocal.withInitial(WeakHashMap::new);
+    private static final Cache<IBlockAccess, RegionCache> regionMetaCache = CacheBuilder
+    		.newBuilder()
+    		.weakKeys()
+    		.weakValues()
+    		.expireAfterWrite(10, TimeUnit.SECONDS)
+    		.build();
     
     private final Map<ICTMTexture<?>, ITextureContext> contextMap = Maps.newIdentityHashMap();
     private final TObjectLongMap<ICTMTexture<?>> serialized = new TObjectLongCustomHashMap<>(new IdentityHashingStrategy<>());
 
-    public RenderContextList(IBlockState state, Collection<ICTMTexture<?>> textures, IBlockAccess world, BlockPos pos) {
-        world = regionMetaCache.get().computeIfAbsent(world, w -> new RegionCache(pos, 2, w));
+    public RenderContextList(IBlockState state, Collection<ICTMTexture<?>> textures, final IBlockAccess world, BlockPos pos) {
+    	IBlockAccess cachedWorld = world;
+    	try {
+    		cachedWorld = regionMetaCache.get(world, () -> new RegionCache(pos, 2, world));
+    	} catch (ExecutionException e) {
+    		CTM.logger.error(e);
+    		// No-op, world reference remains as passed in
+    	}
         for (ICTMTexture<?> tex : textures) {
             ITextureType type = tex.getType();
-            ITextureContext ctx = type.getBlockRenderContext(state, world, pos, tex);
+            ITextureContext ctx = type.getBlockRenderContext(state, cachedWorld, pos, tex);
             if (ctx != null) {
                 contextMap.put(tex, ctx);
             }
