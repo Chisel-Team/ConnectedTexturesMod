c16b15479714c43fc1b94be130d7b80bcb61a8f4
MsRandom <ashley@msrandom.net>
2023-01-23 01:07:35 +0300

    Fix fallback render layer check

--- src/main/java/team/chisel/ctm/client/util/CTMPackReloadListener.java
+++ src/main/java/team/chisel/ctm/client/util/CTMPackReloadListener.java
@@ -1,21 +1,6 @@
 package team.chisel.ctm.client.util;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
-import javax.annotation.Nullable;
-
-import org.apache.commons.lang3.tuple.Pair;
-
-import com.google.common.collect.Maps;
 import com.mojang.datafixers.util.Unit;
-
 import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
 import it.unimi.dsi.fastutil.objects.Object2BooleanOpenHashMap;
 import lombok.RequiredArgsConstructor;
@@ -23,22 +8,21 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.MultiPartBakedModel;
-import net.minecraft.client.resources.model.WeightedBakedModel;
-import net.minecraft.core.Holder;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.ChunkRenderTypeSet;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
-import net.minecraftforge.registries.ForgeRegistries;
 import team.chisel.ctm.client.model.AbstractCTMBakedModel;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class CTMPackReloadListener extends SimplePreparableReloadListener<Unit> {
 
@@ -60,8 +44,6 @@ public class CTMPackReloadListener extends SimplePreparableReloadListener<Unit> 
         TextureMetadataHandler.INSTANCE.invalidateCaches();
         AbstractCTMBakedModel.invalidateCaches();
     }
-
-    private static final Map<Holder.Reference<Block>, Predicate<RenderType>> blockRenderChecks = Maps.newHashMap();
 
     @RequiredArgsConstructor
     public static class CachingLayerCheck implements Predicate<RenderType> {
@@ -98,38 +80,8 @@ public class CTMPackReloadListener extends SimplePreparableReloadListener<Unit> 
         }
     }
 
-    private static final Field _blockRenderChecks = ObfuscationReflectionHelper.findField(ItemBlockRenderTypes.class, "BLOCK_RENDER_TYPES");
-    private static final MethodHandle _fancyGraphics;
-    static {
-        try {
-            _fancyGraphics = MethodHandles.lookup().unreflectGetter(ObfuscationReflectionHelper.findField(ItemBlockRenderTypes.class, "f_109277_"));
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private ChunkRenderTypeSet getExistingRenderCheck(Block block) {
-        try {
-            return ((Map<Holder.Reference<Block>, ChunkRenderTypeSet>) _blockRenderChecks.get(null)).get(ForgeRegistries.BLOCKS.getDelegateOrThrow(block));
-        } catch (IllegalArgumentException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+    @SuppressWarnings("removal")
     public static boolean canRenderInLayerFallback(BlockState state, RenderType type) {
-        Block block = state.getBlock();
-        if (block instanceof LeavesBlock) {
-            try {
-                return ((boolean) _fancyGraphics.invokeExact()) ? type == RenderType.cutoutMipped() : type == RenderType.solid();
-            } catch (Throwable e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            java.util.function.Predicate<RenderType> rendertype;
-            synchronized (ItemBlockRenderTypes.class) {
-                rendertype = blockRenderChecks.get(ForgeRegistries.BLOCKS.getDelegateOrThrow(block));
-            }
-            return rendertype != null ? rendertype.test(type) : type == RenderType.solid();
-        }
+        return ItemBlockRenderTypes.getRenderLayers(state).contains(type);
     }
 }
